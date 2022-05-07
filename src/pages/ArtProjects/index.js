6155dd387088d2b68fedbe9c5d4f497c57395b31
unknown <timoosterlee1@live.nl>
2022-05-07 15:54:35 +0200

    PBR house project blender

--- src/pages/ArtProjects/index.js
+++ src/pages/ArtProjects/index.js
@@ -7,6 +7,7 @@ import ImageArtProjectItem from "../../components/ImageArtProjectItem"
 import chessboard from '../../assets/images/RenderedChessBoard.jpg';
 import bedroomEvee from '../../assets/images/BedroomEvee.jpg'
 import bedroomCycles from '../../assets/images/BedroomCycles.jpg'
+import pbrHouse from '../../assets/images/House.jpg'
 
 function ArtProjects() {
   return (
@@ -104,7 +105,7 @@ function ArtProjects() {
             title='Chessboard'
             imagepath={chessboard}
             alt="Chessboard"
-            info="Modellen zelf gemaakt met behulp van referentie fotos. Geleerd hoe je door loop cuts, extrude, scale en rotate modellen kan maken, compositie en licht en metaball voor wolken.  "
+            info="Modellen zelf gemaakt met behulp van referentie fotos. Geleerd hoe je door loop cuts, extrude, scale en rotate modellen kan maken, compositie, licht en metaball voor wolken.  "
             tech={['Blender']}
             />
             <ImageArtProjectItem
@@ -119,6 +120,13 @@ function ArtProjects() {
             imagepath={bedroomCycles}
             alt="Bedroom"
             info="Cloth simulation, Bump maps, displacement, roughness textures, ambient occlusion, normal maps, Sculpting, Hair particles, irradiance volume, Principled volume world texture, modelling carpet/bed/cushions. Changing materials with Shader editor. "
+            tech={['Blender']}
+            />
+            <ImageArtProjectItem
+            title='Photorealism House'
+            imagepath={pbrHouse}
+            alt="PBRHouse"
+            info="Poging om een huis er realistisch uit te laten zien door gebruik te maken van archimesh, PBR materials in seperate RGB channels, Fluid simulation with foam, low poly rocks, displacement/solidify/ modifiers, Boolean modifiers, Curve geometry for railings."
             tech={['Blender']}
             />
         </div>    
