db31795f93bd72b006705bd8edb700f6330f0d7b
unknown <timoosterlee1@live.nl>
2022-05-12 09:18:51 +0200

    title change

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div id='home' className='hero-container'>   
       <h1 className='hero-title'>Tim Oosterlee</h1>
-      <p className='hero-subtitle'>Developer</p>   
+      <p className='hero-subtitle'>Software Engineer</p>   
       <div className="arrow">                
                 <span></span>  
       </div>  
